69b1a98c061126e50dc2016fedf6b043ee2fa961
Adarsha Krishna <adarshakrishna2003@gmail.com>
2024-10-15 21:23:11 +0530

    ExamResultSummary UI

---
+++ app/src/main/java/com/example/examease/quiz/ExamResultSummary.java
@@ -0,0 +1,17 @@
+package com.example.examease.quiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.examease.R;
+
+public class ExamResultSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_result_summary);
+
+    }
+}
